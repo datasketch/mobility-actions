4b8ea31227b92d384ca5ca3115c1741b8bf3c25a
David Daza <03dazal@gmail.com>
2020-08-05 12:07:27 -0500

    Add app

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,6 +2,8 @@ const trigger = document.getElementById('menu-trigger');
 const open = document.getElementById('menu-open');
 const close = document.getElementById('menu-close');
 const content = document.getElementById('menu-content');
+const moveTo = new MoveTo();
+const navTriggers = document.getElementsByClassName('js-trigger');
 
 trigger.addEventListener('click', function () {
   open.classList.toggle('hidden');
@@ -10,3 +12,9 @@ trigger.addEventListener('click', function () {
   content.classList.toggle('hidden');
   content.classList.toggle('flex');
 });
+
+for (const trigger of navTriggers) {
+  moveTo.registerTrigger(trigger);
+}
+
+iFrameResize({ heightCalculationMethod: 'lowestElement' }, '#app');
